59bcb43f291c98a916593ecc49e83dbecbc57355
joseandre25 <andrerabelo2503@gmail.com>
2023-10-18 19:52:03 -0300

    :pencil: teste sistema branch/feature

--- app/page.tsx
+++ app/page.tsx
@@ -87,7 +87,7 @@ export default function Home() {
           </h2>
           <p>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
-            Mais um teste de CI (build e deploy)
+            Teste com uma branch separada pra feature
           </p>
         </a>
       </div>
