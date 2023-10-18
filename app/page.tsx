cd4389ea587dd936a80a339c30d329988588ea79
joseandre25 <andrerabelo2503@gmail.com>
2023-10-18 19:30:18 -0300

    :lock: test

--- app/page.tsx
+++ app/page.tsx
@@ -87,7 +87,7 @@ export default function Home() {
           </h2>
           <p>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
-            José André Teste Teeeeeste
+            Mais um teste de CI (build e deploy)
           </p>
         </a>
       </div>
