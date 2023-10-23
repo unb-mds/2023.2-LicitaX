5ee28b542937dd03bcc0d9579832225d72b12560
joseandre25 <andrerabelo2503@gmail.com>
2023-10-23 19:05:19 -0300

    :repeat: novos components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import MainLayout from '../layouts/MainLayout';
+import Head from "next/head";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <MainLayout>
-      <p>
-          Dados públicos extraídos de diários oficiais municipais da Associação
-          dos Municípios do Estado do Ceará (APRECE). Todo o conteúdo do site está disponível
-          sob a licença Creative Commons Atribuição 4.0 Internacional, o que
-          significa que pode ser compartilhado e reutilizado para trabalhos
-          derivados, desde que citada a fonte.
-        </p>
-    </MainLayout>
+    <main>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <MainLayout>
+        <Footer />
+      </MainLayout>
+    </main>
   );
 }
