5d48a4eb55e3a30244329afc4e7917bdcdd6343b
joseandre25 <andrerabelo2503@gmail.com>
2023-10-23 22:03:25 -0300

    :pencil: layout inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,16 @@ import MainLayout from '../layouts/MainLayout';
 
 export default function Home() {
   return (
-    <MainLayout>
-      <Footer />
-    </MainLayout>
+    <div className="flex-col">
+      <div className="bg-gray-900 h-16 w-screen">
+        {/* Conteúdo da Barra Superior */}
+      </div>
+      <div className="bg-teal-500 w-16 h-screen">
+        {/* Conteúdo da Barra à Esquerda */}
+      </div>
+      <div className="flex-1">
+        <Footer />
+      </div>
+    </div>
   );
 }
