26cb9c19f2225713d4475fc74e5d9f803f8753ec
joseandre25 <andrerabelo2503@gmail.com>
2023-12-04 16:52:18 -0300

    :pencil: busca na API

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head />
+      <link rel="shortcut icon" href="/favicon.ico" />
       <Component {...pageProps} />
     </>
   );
